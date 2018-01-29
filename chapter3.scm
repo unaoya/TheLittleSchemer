ada5617148edf87cbe5d680067a81d88ea41c54f
unaoya <umezakinaoya@gmail.com>
2018-01-30 00:59:29 +0900

    p53

--- chapter3.scm
+++ chapter3.scm
@@ -29,5 +29,33 @@
 
 (insertR 'topping 'fudge '(ice cream with fudge for dessert))
 (insertR 'jalapeno 'and '(tacos tamales and salsa))
-(insertR 'e 'd '(a b c d f g d h)
-   
+(insertR 'e 'd '(a b c d f g d h))
+
+(define insertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old) (cons new lat))
+	    (else (cons (car lat) (insertL new old (cdr lat)))))))))
+
+(insertL 'a 'b '(a b c d))
+(insertL 'a 'b '(c d b a b))
+
+(define subst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((eq? (car lat) old) (cons new (cdr lat)))
+	    (else (cons (car lat) (subst new old (cdr lat)))))))))
+(subst 'topping 'fudge '(ice cream with fudge for dessera))
+
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else (cond
+	    ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+	    (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
+(subst2 'vanilla 'chocolate 'banana '(banana ice cream with chocolate topping))
