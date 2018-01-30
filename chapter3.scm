720fe0c75b881049bfb3a941c4c9c3e0b1c1614e
unaoya <umezakinaoya@gmail.com>
2018-01-31 01:10:14 +0900

    finish chapter3

--- chapter3.scm
+++ chapter3.scm
@@ -59,3 +59,44 @@
 	    ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
 	    (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
 (subst2 'vanilla 'chocolate 'banana '(banana ice cream with chocolate topping))
+
+
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) a) (multirember a (cdr lat)))
+       (else (cons (car lat) (multirember a (cdr lat)))))))))
+(multirember 'cup '(coffee cup tea cup and hick cup))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old) (cons old (cons new (multiinsertR new old (cdr lat)))))
+       (else (cons (car lat) (multiinsertR new old (cdr lat)))))))))
+(multiinsertR 'a 'b '(a b b c d a))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+       (else (cons (car lat) (multiinsertL new old (cdr lat)))))))))
+(multiinsertL 'fried 'fish '(chips and fish or fish and fried))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) (quote ()))
+     (else
+      (cond
+       ((eq? (car lat) old) (cons new (multisubst new old (cdr lat))))
+       (else (cons (car lat) (multisubst new old (cdr lat)))))))))
+(multisubst 'a 'b '(a b a c a b a))
